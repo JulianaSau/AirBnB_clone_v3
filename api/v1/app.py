52cfd843b618f9b167e6e911c8f3ca40f6b0079d
Shaker Sharabi <sheko100@icloud.com>
2024-03-30 20:48:36 +0200

    Add not found handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,20 @@
 """Module that defines the root route
 """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles  not found resources
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
